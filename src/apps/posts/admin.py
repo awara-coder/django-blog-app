1f224f172652c57a16713f4f777cb083f6fdeb14
Sagar <gupta-sagar@outlook.com>
2018-12-12 19:16:35 +0530

    Registered Post to admin site

--- src/apps/posts/admin.py
+++ src/apps/posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title", "published",]
+    list_filter = ["last_modified", "published"]
+    search_fields = ["title", "content"]
+    class Meta:
+        model = Post
+    
+
+admin.site.register(Post, PostModelAdmin)
