0f221f3ecebeca4eadb76f9af93281ee6f69416f
Sagar <gupta-sagar@outlook.com>
2018-12-12 18:19:39 +0530

    Added apps folder to sys.path

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -15,7 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+# Adding apps folder to system path
+import sys
+sys.path.append(os.path.join(BASE_DIR,'apps/'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
