aa761089856b75412c1016823b4bb4b76423a68c
Sagar <gupta-sagar@outlook.com>
2018-12-12 19:03:41 +0530

    Added Post model

--- src/apps/posts/models.py
+++ src/apps/posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length = 150)
+    content = models.TextField()
+    published = models.DateTimeField(auto_now = False, auto_now_add = True)
+    last_modified = models.DateTimeField(auto_now = True, auto_now_add = False)
+
+    def __str__(self):
+        return self.title
